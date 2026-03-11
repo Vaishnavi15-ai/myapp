89c3197623daa75f9df51d8562a088e03e43ada0
Vaishnavi15-ai <vaishnaviamu190@gmail.com>
2026-03-11 12:28:02 +0530

    Update register.jsx

--- src/register.jsx
+++ src/register.jsx
@@ -17,7 +17,7 @@ function Reg()
     {
         try
         {
-            //const res = await axios.post("https://localhost:8080/reg",data)
+            //const res = await axios.post("https://cabsystems.onrender.com/register",data)
             const res = await axios.post(
             "https://cabsystemsms-1.onrender.com/register",
                 data
